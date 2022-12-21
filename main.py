1b536dfb891eae175d2379ebc5173bc8e681c3d0
Ashwin <ashes192000@gmail.com>
2022-12-22 03:16:05 +0530

    Fix requirementsa and add env

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import pymongo
 import json
 import jwt
 import bcrypt
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 client = pymongo.MongoClient('mongodb://localhost:27017/')
-db = client["flask_app"]
+db = client[os.getenv('DATABASE')]
 app = Flask(__name__)
 
 def hash_password(password):
@@ -16,7 +19,7 @@ def hash_password(password):
 @app.route('/login',methods=['GET'])
 def login():
     try:
-        users_collection = db["user"]
+        users_collection = db[os.getenv('USER_COLLECTION')]
         data = request.get_json()
         username = data['username']
         password = data['password']
@@ -27,7 +30,6 @@ def login():
         if not validate:
             response = {'message': 'User not loggeed IN'}
             return make_response(jsonify(response), 404)
-        print("success")
         response = {'message': 'User logged IN successfully'}
         return make_response(jsonify(response), 200)
     except:
@@ -42,7 +44,7 @@ def register():
         password = data['password']
         hashedPassword = hash_password(password)
         agent_id = data['agent_id']
-        users_collection = db["user"]
+        users_collection = db[os.getenv('USER_COLLECTION')]
         new_user={'username':username,'password':hashedPassword,'agent_id':agent_id}
         users_collection.insert_one(new_user)
         response = {'message': 'User added successfully'}
@@ -57,7 +59,7 @@ def update():
         data = request.get_json()
         updateKey = request.args.get('id')
         updaterField = request.args.get('updater')
-        users_collection = db["user"]
+        users_collection = db[os.getenv('USER_COLLECTION')]
         if updaterField == 'username':
             users_collection.update_one({"agent_id": updateKey}, {"$set": {"username": new_username}})
         else:
@@ -73,7 +75,7 @@ def update():
 def delete():
     try:
         agentID = request.args.get('id')
-        users_collection = db["user"]
+        users_collection = db[os.getenv('USER_COLLECTION')]
         users_collection.delete_one({"agent_id": agentID})
         response = {'message': 'User deleted successfully'}
         return make_response(jsonify(response), 200)
